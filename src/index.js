45107a1b1b9899e5d4a1e1de2624b5c83cb37ca4
Aljon Cadiz <AljonC@magenic.com>
2018-09-01 01:39:33 +0800

    webpack dev-prod with react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-var message = require('./script1');
-
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
-
-document.write(`this is a formatted ES6 ${message}`);
+import App from "./App";
